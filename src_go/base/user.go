3f364f0159c05b22540885313f13e742183f89eb
徐晨阳 <xuender@gmail.com>
2014-09-25 14:31:29 +0800

    日志修改

--- src_go/base/user.go
+++ src_go/base/user.go
@@ -1,23 +1,25 @@
 package base
 
 import "errors"
+
 // 用户
-type User struct{
-  Id        int64
-  Nick      string
-  Email     string
-  Password  string
+type User struct {
+	Id       int64
+	Nick     string
+	Email    string
+	Password string
 }
+
 // 用户密码教研
-func (u User) check(password string) bool{
-  return u.Password == password
+func (u User) check(password string) bool {
+	return u.Password == password
 }
 
 // TODO 使用数据库替换
 // 查找用户
-func FindUser(nick string) (User, error){
-  if nick == "ender" {
-    return  User{Id:2, Nick:"ender", Email:"xxx@xxx", Password:"d9b1d7db4cd6e70935368a1efb10e377"}, nil
-  }
-  return User{}, errors.New("用户没有找到")
+func FindUser(nick string) (User, error) {
+	if nick == "ender" {
+		return User{Id: 2, Nick: "ender", Email: "xxx@xxx", Password: "d9b1d7db4cd6e70935368a1efb10e377"}, nil
+	}
+	return User{}, errors.New("用户没有找到")
 }
