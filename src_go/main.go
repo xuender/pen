1473fbc1a1c3008ca285d6970d1a9e077a3472ce
徐晨阳 <xuender@gmail.com>
2014-09-25 14:19:32 +0800

    自动格式化

--- src_go/main.go
+++ src_go/main.go
@@ -4,31 +4,31 @@ import (
 	//db "./db"
 	"./base"
 	"code.google.com/p/go.net/websocket"
+	log "github.com/Sirupsen/logrus"
 	"github.com/go-martini/martini"
-  log "github.com/Sirupsen/logrus"
 	"github.com/martini-contrib/gzip"
 	"github.com/martini-contrib/render"
-  //"./utils"
+	//"./utils"
 )
 
 func main() {
-  log.Info("启动")
-  // 检查文件完整性
-  //err := base.CheckFiles()
-  //if(err!=nil){
-  //  log.Error(err)
-  //  return
-  //}
-  m := martini.Classic()
-  // gzip支持
-  m.Use(gzip.All())
-  // 模板支持
-  m.Use(render.Renderer())
-  // websocket 支持
-  m.Get("/ws", websocket.Handler(base.WsHandler).ServeHTTP)
-  m.NotFound(func(r render.Render) {
-    r.HTML(404, "404", nil)
-  })
-  m.Run()
-  log.Info("退出")
+	log.Info("启动")
+	// 检查文件完整性
+	//err := base.CheckFiles()
+	//if(err!=nil){
+	//  log.Error(err)
+	//  return
+	//}
+	m := martini.Classic()
+	// gzip支持
+	m.Use(gzip.All())
+	// 模板支持
+	m.Use(render.Renderer())
+	// websocket 支持
+	m.Get("/ws", websocket.Handler(base.WsHandler).ServeHTTP)
+	m.NotFound(func(r render.Render) {
+		r.HTML(404, "404", nil)
+	})
+	m.Run()
+	log.Info("退出")
 }
