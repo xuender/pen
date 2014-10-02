be0cc7d97e3adc9ee7fb611306672d77ac11ec38
徐晨阳 <xuender@gmail.com>
2014-10-02 14:20:53 +0800

    多线程分发数据

--- src_go/base/user.go
+++ src_go/base/user.go
@@ -1,73 +1,73 @@
 package base
 
 import (
-  "code.google.com/p/go.net/websocket"
-  "errors"
-  log "github.com/Sirupsen/logrus"
 	"../utils"
-  "time"
+	"code.google.com/p/go.net/websocket"
+	"errors"
+	log "github.com/Sirupsen/logrus"
+	"time"
 )
 
 // 用户
 type User struct {
-  BaseObject
-  // 性别
-  Gender    int64
-  // 昵称
-  Nick      string
-  Email     string
-  Password  string  `json:"-"`
+	BaseObject
+	// 性别
+	Gender int64
+	// 昵称
+	Nick     string
+	Email    string
+	Password string `json:"-"`
 }
 
 // 用户密码教研
 func (u *User) check(password string) bool {
-  return u.Password == password
+	return u.Password == password
 }
+
 // 根据track计算token
-func (u *user) getToken(track string) string{
-  return utils.Md5(tract + utils.Md5(time.Now().Format("2006-01-02") + u.Password))
+func (u *User) getToken(track string) string {
+	return utils.Md5(track + utils.Md5(time.Now().Format("2006-01-02")+u.Password))
 }
 
 // 查找用户
 func UserRead(nick string) (User, error) {
-  var user User
-  log.Debug(user.Id)
-  db.Where("nick = ?", nick).First(&user)
-  log.Debug(user.Id)
-  if user.Id > 0 {
-    return user, nil
-  }
-  return User{}, errors.New("用户没有找到")
+	var user User
+	log.Debug(user.Id)
+	db.Where("nick = ?", nick).First(&user)
+	log.Debug(user.Id)
+	if user.Id > 0 {
+		return user, nil
+	}
+	return User{}, errors.New("用户没有找到")
 }
 
 // 获取全部用户
 func userAllEvent(data *string, ws *websocket.Conn, session Session) {
-  //TODO 权限认证
-  var users []User
-  db.Find(&users)
-  send(ws, Code, 用户列表, users)
+	//TODO 权限认证
+	var users []User
+	db.Find(&users)
+	send(ws, Code, 用户列表, users)
 }
-
 func init() {
-  RegisterEvent(Code, 用户列表, userAllEvent)
-  // 数据库初始化
-  db.AutoMigrate(&User{})
-  db.Model(&User{}).AddUniqueIndex("idx_user_nick", "nick")
-  // 创建管理员
-  var count int
-  db.Model(User{}).Count(&count)
-  if count == 0 {
-    e := User{
-      Nick: "ender",
-      Email: "xxx@xxx",
-      Password: "d9b1d7db4cd6e70935368a1efb10e377",
-    }
-    log.WithFields(log.Fields{
-      "id": e.Id,
-    }).Debug("增加管理员")
-    db.Create(&e)
-    log.WithFields(log.Fields{
-      "id": e.Id,
-    }).Debug("增加管理员之后")
-  }
+	RegisterEvent(Code, 用户列表, userAllEvent)
+	// 数据库初始化
+	db.AutoMigrate(&User{})
+	db.Model(&User{}).AddUniqueIndex("idx_user_nick", "nick")
+	// 创建管理员
+	var count int
+	db.Model(User{}).Count(&count)
+	if count == 0 {
+		e := User{
+			Nick:     "ender",
+			Email:    "xxx@xxx",
+			Password: "d9b1d7db4cd6e70935368a1efb10e377",
+		}
+		log.WithFields(log.Fields{
+			"id": e.Id,
+		}).Debug("增加管理员")
+		db.Create(&e)
+		log.WithFields(log.Fields{
+			"id": e.Id,
+		}).Debug("增加管理员之后")
+	}
 }
